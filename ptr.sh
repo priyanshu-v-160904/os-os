e7cb3cf602b2d401d746a03578937c5de4b28fb4
Priyanshu Verma <priyanshuloco007@gmail.com>
2025-04-02 11:57:25 +0530

    Add files via upload

---
+++ ptr.sh
@@ -0,0 +1,12 @@
+for ((i=0;i<5;i++))
+do
+	for ((k=0;k<=4-i;k++))
+	do
+	printf " "
+	done
+	for ((j=0;j<=i;j++))
+		do
+		printf "*"
+		done
+	printf "\n"
+done
---
+++ ptr2.sh
@@ -0,0 +1,16 @@
+for ((i=0;i<5;i++))
+do
+        for ((k=0;k<=4-i;k++))
+        do
+        printf " "
+        done
+        for ((j=0;j<(2*i)-1;j++))
+                do
+                printf "*"
+                done
+        printf "\n"
+done
+
+
+
+
